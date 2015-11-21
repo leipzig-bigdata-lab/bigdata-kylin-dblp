37c2da454d742bcf325e0c5e0762ce7833275775
Klemens Schölhorn <klemens@schoelhorn.eu>
2015-11-21 18:03:26 +0100

    Implement output to hdfs

--- src/main/java/bigdata/dblp/Parser.java
+++ src/main/java/bigdata/dblp/Parser.java
@@ -1,12 +1,14 @@
 package bigdata.dblp;
 
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.Set;
@@ -16,6 +18,11 @@ import javax.xml.stream.XMLResolver;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
 import bigdata.dblp.records.Collection;
 import bigdata.dblp.records.Publication;
@@ -151,36 +158,45 @@ public class Parser {
     }
 
     public static void main(String[] args) {
-        String file;
-
-        if(args.length < 1) {
-            System.err.println("Usage: dblp-importer /local/dblp.xml|stdin");
+        if(args.length < 2) {
+            System.err.println("Usage: dblp-importer /local/dblp.xml|stdin /path/in/hdfs");
             System.exit(1);
             return;
         }
 
-        file = args[0];
+        String file = args[0];
+        Path hdfsPath = new Path(args[1]);
 
         InputStream dblpInput;
-        Writer publicationOutput;
-        Writer collectionOutput;
         try {
             if("stdin".equals(file)) {
                 dblpInput = System.in;
             } else {
                 dblpInput = new FileInputStream(file);
             }
-            publicationOutput = new OutputStreamWriter(new FileOutputStream(file + "-pub"), StandardCharsets.UTF_8);
-            collectionOutput = new OutputStreamWriter(new FileOutputStream(file + "-col"), StandardCharsets.UTF_8);
-        } catch (FileNotFoundException e) {
-            System.err.println("File or Directory not found: " + e);
+        } catch(FileNotFoundException e) {
+            System.err.println("Input file not found: " + e);
             System.exit(2);
             return;
         }
 
         try {
-            parse(dblpInput, publicationOutput, collectionOutput);
-        } catch (XMLStreamException | IOException e) {
+            Configuration conf = new Configuration();
+            FileSystem fs = FileSystem.get(new URI("hdfs://localhost"), conf);
+            fs.mkdirs(hdfsPath);
+
+            FSDataOutputStream publicationFile = fs.create(new Path(hdfsPath, "dblp-publications.csv"));
+            FSDataOutputStream collectionFile = fs.create(new Path(hdfsPath, "dblp-collections.csv"));
+
+            try(Writer publicationOutput = new BufferedWriter(new OutputStreamWriter(publicationFile, StandardCharsets.UTF_8));
+                Writer collectionOutput = new BufferedWriter(new OutputStreamWriter(collectionFile, StandardCharsets.UTF_8))) {
+                parse(dblpInput, publicationOutput, collectionOutput);
+            }
+        } catch(IOException | URISyntaxException e) {
+            System.err.println("Error writing to hdfs: " + e);
+            System.exit(2);
+            return;
+        } catch(XMLStreamException e) {
             System.err.println("Error while parsing dblp.xml: " + e);
             System.exit(3);
             return;
