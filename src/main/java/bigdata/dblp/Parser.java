c62896d02be10268319af615b947e35b4db57556
Klemens Schölhorn <klemens@schoelhorn.eu>
2015-11-30 15:59:45 +0100

    Fix fat jar by defining explicit path for classes with same name

--- src/main/java/bigdata/dblp/Parser.java
+++ src/main/java/bigdata/dblp/Parser.java
@@ -222,6 +222,8 @@ public class Parser {
 
         try {
             Configuration conf = new Configuration();
+            conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+            conf.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
             FileSystem fs = FileSystem.get(new URI("hdfs://localhost"), conf);
             fs.mkdirs(hdfsPath);
 
