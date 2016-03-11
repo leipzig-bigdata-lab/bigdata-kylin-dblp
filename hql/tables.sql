f75d72caec751f0fb4818ee1001c3b5202e792a2
Klemens Schölhorn <klemens@schoelhorn.eu>
2016-03-11 15:53:49 +0100

    Update table definitions to latest pub-formatter output

--- hql/tables.sql
+++ hql/tables.sql
@@ -1,77 +1,76 @@
 use ${hiveconf:DB_SCHEMA};
 
-drop table if exists Timeinfo;
-drop table if exists TimeInfo;
-create external table if not exists timeinfo (
+create external table timeinfo (
 	time_id int,
-	year int, 
-	month int, 
-	day int, 
-	decade int)
+	year int,
+	month int,
+	day int,
+	decade int
+)
 row format delimited
 fields terminated by "," escaped by "\\"
 lines terminated by "\n"
 stored as textfile
 location '${hiveconf:TABLE_PATH}/time';
 
-drop table if exists Venue_Series;
-create external table if not exists Venue_Series (
+create external table venue_series (
 	venue_series_id int,
-	name String, 
-	description String)
+	name string,
+	dblp_key string
+)
 row format delimited
 fields terminated by "," escaped by "\\"
 lines terminated by "\n"
 stored as textfile
 location '${hiveconf:TABLE_PATH}/venue_series';
 
-drop table if exists Title;
-create external table if not exists Title (
+create external table title (
 	title_id int,
-	title String)
+	title string,
+	ignore1 string
+)
 row format delimited
 fields terminated by "," escaped by "\\"
 lines terminated by "\n"
 stored as textfile
 location '${hiveconf:TABLE_PATH}/title';
 
-drop table if exists Author;
-create external table if not exists Author (
+create external table author (
 	author_id int,
-	name String,
-	institution_id int)
+	name string
+)
 row format delimited
 fields terminated by "," escaped by "\\"
 lines terminated by "\n"
 stored as textfile
 location '${hiveconf:TABLE_PATH}/author';
 
-drop table if exists Document_Type;
-create external table if not exists Document_Type (
+create external table document_type (
 	type_id int,
-	name String, 
-	description String)
+	name string,
+	description string
+)
 row format delimited
 fields terminated by "," escaped by "\\"
 lines terminated by "\n"
 stored as textfile
 location '${hiveconf:TABLE_PATH}/document_type';
 
-drop table if exists Publication;
-create external table if not exists Publication (
-	publication_id String,
+create external table publication (
+	publication_id int,
 	title_id int,
 	type_id int,
 	time_id int,
-	venue_series_id int, 
-	citings_dblp int, 
-	citings_gs int, 
-	citings_acm int, 
-	citings_acm_self int)
+	venue_series_id int,
+	citings_dblp int,
+	ignore1 int,
+	ignore2 int,
+	ignore3 int,
+	author_id int,
+	ignore4 string
+)
 row format delimited
 fields terminated by "," escaped by "\\"
 lines terminated by "\n"
 stored as textfile
 location '${hiveconf:TABLE_PATH}/publication';
-
-use default;
